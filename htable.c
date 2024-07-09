43cf973906e9b7b5cd23fb127c102b46dcbcd4b2
mxlanDev <milan.minic@proton.me>
2024-07-09 15:34:37 +0200

    hashtable workable

--- htable.c
+++ htable.c
@@ -3,8 +3,7 @@
 HTable* htableInit(){
   HTable* table = (HTable*)malloc(sizeof(HTable));
   table->size = INITIAL_SIZE;
-  HElement* array = (HElement*)malloc(sizeof(HElement)*table->size);
-  DList* list = dlistInit();
+  HElement* array = (HElement*)calloc(INITIAL_SIZE,sizeof(HElement));
   return table;
 }
 
@@ -12,14 +11,47 @@ HElement* htableSearch(HTable* table, char* key, unsigned int (*hashFunction)(co
   unsigned int location = (*hashFunction)(key)%(unsigned int)table->size;
   return &table->array[location];
 }
-void htableAdd(HTable* table, char* key,void* ptr, unsigned int *(hashFunction)(const char* key)){
-     
+void htableAdd(HTable* table, char* key,void* ptr, unsigned int (*hashFunction)(const char* key)){
+  unsigned int location = (*hashFunction)(key)%(unsigned int)table->size;
+  if(table->array[location].ptr){
+   
+    return;
+  }
+  table->array[location].key = key;
+  table->array[location].hash = location;
+  table->array[location].ptr = ptr;
+  return;
 }
 
-void htableRemove(HTable* table, char* key, void* ptr, unsigned int *(hashFunction)(const char* key)){
-
+void* htableRemove(HTable* table, char* key, unsigned int (*hashFunction)(const char* key)){
+  unsigned int location = (*hashFunction)(key)%(unsigned int)table->size;
+  HElement element = table->array[location];
+  if(table->array[location].ptr)
+    if(*key==element->key&&!strcmp(key,element.key)){//???
+      void* ptr = element.ptr;
+      memset(table + location,0,sizeof(HElement));
+      return ptr;
+    }
+  return NULL;
 }
 
-void htableExtend(HTable* table, char* key, void* ptr, unsigned int *(hashFunction)(const char* key)){
-
+void htableExtend(HTable* table,unsigned int (*hashFunction)(const char* key)){
+  unsigned int newSize = (table->size-1<<1)+1;
+  HElement* new = (HElement*)calloc(newSize ,sizeof(HElement));
+  for(int i = 0;i<table->size;i++){
+    if(!table->array[i].ptr)continue;
+    unsigned int location = (*hashFunction)(table->array[i].key)%(unsigned int)newSize;
+    if(new[location].ptr){
+      free(new);
+      newSize = (newSize-1<<1)+1;
+      new = (HElement*)calloc(newSize ,sizeof(HElement));
+      i=0;
+      continue;
+    }
+    new[location].ptr = table->array[i].ptr;
+    new[location].key = table->array[i].key;
+    new[location].hash = table->array[i].hash;
+  }
+  free(table->array);
+  table->array = new;
 }
--- htable.h
+++ htable.h
@@ -1,4 +1,5 @@
-#include "doublylinkedlist.h"
+#include <string.h>
+#include <stdlib.h>
 
 #define INITIAL_SIZE 10
 
@@ -11,11 +12,10 @@ typedef struct{
 typedef struct{
   HElement* array;
   int size;
-  DList* list;
 }HTable;
 
 HTable* htableInit();
 HElement* htableSearch(HTable* table, char* key, unsigned int (*hashFunction)(const char* key));
-void htableAdd(HTable* table, char* key, void* ptr, unsigned int *(hashFunction)(const char* key));
-void htableRemove(HTable* table, char* key, void* ptr,  unsigned int *(hashFunction)(const char* key));
-void htableExtend(HTable* table, char* key, void* ptr,  unsigned int *(hashFunction)(const char* key));
+void htableAdd(HTable* table, char* key, void* ptr, unsigned int (*hashFunction)(const char* key));
+void* htableRemove(HTable* table, char* key,  unsigned int (*hashFunction)(const char* key));
+void htableExtend(HTable* table, unsigned int (*hashFunction)(const char* key));
